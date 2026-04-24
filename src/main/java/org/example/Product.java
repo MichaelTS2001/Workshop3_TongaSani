7da52595af1a0438efa0fe7508bb68d301ff901b
MichaelTS2001 <@Allansoccer_24>
2026-04-24 09:43:25 -0400

    Commit

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,57 @@
+package org.example;
+
+public class Product {
+
+    //properties
+    private String sku;
+    private String productName;
+    private double price;
+    private String department;
+
+    //constructor(s)
+
+    public Product(String sku, String productName, double price, String department) {
+        this.sku = sku;
+        this.productName = productName;
+        this.price = price;
+        this.department = department;
+    }
+
+    //getters and setters
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String toString(){
+        return String.format("%s | %s | $%.2f | %s ", sku, productName, price, department);
+    }
+}
